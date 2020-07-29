f4298afa078bb566d5e284d11ac3e426d9c20973
elsoleiro <elsoleiro@gmail.com>
2020-07-29 17:56:24 +0100

    print values from columns, validate data has been read

--- static/stacked_graph1.js
+++ static/stacked_graph1.js
@@ -4,6 +4,16 @@ const width = +svg.attr('width');
 const height = +svg.attr('height');
 
 d3.csv('static/dataset1.csv').then(data => {
-    console.log(data);
+    data.forEach(d => {
+        d.Dates = d.Dates;
+        d.group_1 = +d.group_1;
+        d.group_2 = +d.group_2;
+        d.group_3 = +d.group_3;
+        d.group_4 = +d.group_4;
+        d.group_5 = +d.group_5;
+        d.group_6 = +d.group_6;
+        console.log(d.Dates);
+        console.log(d.group_1);
+    });
 });
 
