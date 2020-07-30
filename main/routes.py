d78588c6fc0699a887986b94119ca557beedecaf
Bethany Breed <bethany.breed@thameswater.co.uk>
2020-07-30 12:19:46 +0100

    updated bar chart, now appends data

--- main/routes.py
+++ main/routes.py
@@ -5,3 +5,7 @@ site = Blueprint('site', __name__)
 @site.route('/')
 def index():
     return render_template("index.html")
+
+@site.route('/something')
+def something():
+    return render_template('something.html')
--- static/stacked_graph1.js
+++ static/stacked_graph1.js
@@ -3,20 +3,30 @@ const svg = d3.select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
 
-const render = data => {
-    const xScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.group_1)])
-        .range([0, width]);
-    const yScale = d3.scaleBand() 
-        .domain(data.map(d => d.Dates))
-        .range([0, height]);
 
+const render = data => {
+    const yValue = d => d.group_1;
+    const xValue = d => d.Dates;
+
+    const yScale = d3.scaleLinear()
+        .range([height, 0])        
+        .domain([0, d3.max(data, yValue)]);
+
+        const xScale = d3.scaleBand() 
+        .domain(data.map(xValue))
+        .range([0, width]);
+
+     
     svg.selectAll('rect').data(data)
      .enter().append('rect')
-        .attr('y', d => yScale(d.Dates))
-        .attr('width', d => xScale(d.group_1))
-        .attr('height', yScale.bandwidth())
+        .attr('y', d => xScale(xValue(d)))
+        .attr('height', d => yScale(yValue(d)))
+        .attr('width', xScale.bandwidth())
+
 };
+
+
+
 
 d3.csv('static/dataset1.csv').then(data => {
     data.forEach(d => {
